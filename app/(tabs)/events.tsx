117bc18f9dbc48d78f8b6918474bbf8e76211581
Aaron Lai <aaronlai@Aarons-Laptop.local>
2026-03-12 23:08:10 +1100

    feat: create basic calendar component

--- app/(tabs)/events.tsx
+++ app/(tabs)/events.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+import { Calendar, type DateType } from "@/components/form/calendar";
 import { ContentSafeArea } from "@/components/layout/content-safe-area";
 import { CreateEventButton } from "@/components/ui/create-event-button";
 import { ThemedText } from "@/components/ui/themed-text";
 
 export default function EventsScreen() {
+	const [date, setDate] = useState<DateType>();
+
 	return (
 		<>
 			<ContentSafeArea>
 				<ThemedText variant="h1">Events</ThemedText>
+				<Calendar
+					mode="single"
+					date={date}
+					onChange={({ date }) => {
+						setDate(date);
+					}}
+					timePicker={true}
+				/>
 			</ContentSafeArea>
 			<CreateEventButton />
 		</>
