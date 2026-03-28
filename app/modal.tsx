b1e5ee3b2cc1b4c80670dc4183496a54468722c9
Aaron Lai <aaronlai@Aarons-Laptop.local>
2026-03-28 20:45:43 +1100

    refactor: host event form in modal

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,23 +1,31 @@
 import { useRouter } from "expo-router";
 import { ArrowLeftIcon } from "phosphor-react-native";
 import { Pressable, View } from "react-native";
+import { CreateEventForm } from "@/components/form/create-event-form";
 import { ContentSafeArea } from "@/components/layout/content-safe-area";
 import { ThemedText } from "@/components/ui/themed-text";
 
 export default function Modal() {
 	const router = useRouter();
+
 	return (
 		<ContentSafeArea>
-			<View className="flex-row justify-start mb-4">
-				<Pressable
-					onPress={() => {
-						router.back();
-					}}
-				>
-					<ArrowLeftIcon size={20} weight="bold" />
-				</Pressable>
+			<View className="mt-4 h-full">
+				{/* Modal Header */}
+				<View className="flex-row justify-start items-center gap-2 mb-4">
+					<Pressable
+						onPress={() => {
+							router.back();
+						}}
+					>
+						<ArrowLeftIcon size={20} weight="bold" />
+					</Pressable>
+					<ThemedText variant="h4">Create new event</ThemedText>
+				</View>
+
+				{/* Modal Content */}
+				<CreateEventForm />
 			</View>
-			<ThemedText variant="h1">Modal</ThemedText>
 		</ContentSafeArea>
 	);
 }
