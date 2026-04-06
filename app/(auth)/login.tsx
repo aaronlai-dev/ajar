f25d078ceef13e100c5ffc75da03543e2f30b1d1
Aaron Lai <aaronlai@Aarons-Laptop.local>
2026-04-06 22:53:30 +1000

    style: style login screen

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,9 +1,11 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "expo-router";
+import { DoorOpenIcon } from "phosphor-react-native";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import { ActivityIndicator, Pressable, Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import { z } from "zod";
+import SubmitFormButton from "@/components/form/submit-form-button";
 import { ThemedTextInput } from "@/components/form/themed-text-input";
 import { ContentSafeArea } from "@/components/layout/content-safe-area";
 import { ThemedText } from "@/components/ui/themed-text";
@@ -48,85 +50,84 @@ const LoginScreen = () => {
 
 	return (
 		<ContentSafeArea>
-			<View className="flex w-full gap-4">
-				<ThemedText variant="h1">ajar</ThemedText>
-				<ThemedText variant="bodyLarge">
-					an app about leaving doors a little open for others
-				</ThemedText>
+			<View className="flex-1 w-full justify-center px-4 pb-8 ">
+				<View className="flex-1 justify-center gap-4">
+					<DoorOpenIcon size={36} weight="regular" />
+					<ThemedText variant="h1">ajar</ThemedText>
+					<ThemedText variant="bodyLarge">
+						in spirit of leaving doors {"\n"}a little open for others
+					</ThemedText>
+				</View>
 
-				<Controller
-					control={control}
-					name="email"
-					render={({ field: { onChange, onBlur, value } }) => (
-						<ThemedTextInput
-							onChange={onChange}
-							onBlur={onBlur}
-							value={value}
-							placeholder="email"
-							keyboardType="email-address"
-							autoCapitalize="none"
-						/>
-					)}
-				/>
-				{errors.email ? (
-					<Text className="text-sm text-red-600 mb-2">
-						{errors.email.message}
-					</Text>
-				) : null}
+				<View className="gap-3">
+					<Controller
+						control={control}
+						name="email"
+						render={({ field: { onChange, onBlur, value } }) => (
+							<ThemedTextInput
+								onChange={onChange}
+								onBlur={onBlur}
+								value={value}
+								placeholder="email"
+								keyboardType="email-address"
+								autoCapitalize="none"
+							/>
+						)}
+					/>
+					{errors.email ? (
+						<Text className="text-sm text-red-600 mb-2">
+							{errors.email.message}
+						</Text>
+					) : null}
 
-				<Controller
-					control={control}
-					name="password"
-					render={({ field: { onChange, onBlur, value } }) => (
-						<ThemedTextInput
-							onChange={onChange}
-							onBlur={onBlur}
-							value={value}
-							placeholder="password"
-							secureTextEntry
-							autoCapitalize="none"
-						/>
-					)}
-				/>
-				{errors.password ? (
-					<Text className="text-sm text-red-600 mb-2">
-						{errors.password.message}
-					</Text>
-				) : null}
+					<Controller
+						control={control}
+						name="password"
+						render={({ field: { onChange, onBlur, value } }) => (
+							<ThemedTextInput
+								onChange={onChange}
+								onBlur={onBlur}
+								value={value}
+								placeholder="password"
+								secureTextEntry
+								autoCapitalize="none"
+							/>
+						)}
+					/>
+					{errors.password ? (
+						<Text className="text-sm text-red-600 mb-2">
+							{errors.password.message}
+						</Text>
+					) : null}
 
-				{message ? (
-					<Text
-						className="text-sm text-center mb-2"
-						style={{
-							color: message.startsWith("Signed up") ? "#10B981" : "#EF4444",
+					{message ? (
+						<Text
+							className="text-sm text-center mb-2"
+							style={{
+								color: message.startsWith("Signed up") ? "#10B981" : "#EF4444",
+							}}
+						>
+							{message}
+						</Text>
+					) : null}
+
+					<SubmitFormButton
+						label="sign in"
+						handleSubmit={handleSubmit(onSignIn)}
+						isPending={isSubmitting}
+					/>
+
+					<Pressable
+						onPress={() => {
+							router.replace("/signup");
 						}}
+						className="items-center"
 					>
-						{message}
-					</Text>
-				) : null}
-
-				<Pressable
-					onPress={handleSubmit(onSignIn)}
-					className="bg-blue-600 rounded p-3 mb-2 items-center"
-					disabled={isSubmitting}
-				>
-					{isSubmitting ? (
-						<ActivityIndicator color="#fff" />
-					) : (
-						<Text className="text-white">Sign in</Text>
-					)}
-				</Pressable>
-
-				<Pressable
-					onPress={() => {
-						router.replace("/signup");
-					}}
-					className="items-center p-3"
-				>
-					<Text className="text-sm text-blue-600">
-						Dont have an account? Create one
-					</Text>
-				</Pressable>
+						<ThemedText variant="bodySmall">
+							first time here? sign up
+						</ThemedText>
+					</Pressable>
+				</View>
 			</View>
 		</ContentSafeArea>
 	);
